9cc3c2f8447f4639d0f2e3607d9be6afcdcae275
zhangyang <zhangyang073@gmail.com>
2016-01-18 02:09:06 +0800

    create demo

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+    entry: [
+        './public/js/src/pages/app.js',
+    ],
+    output: {
+        path: path.resolve(__dirname, './public/js/dist'),
+        publicPath: './public/js',
+        filename: 'app.min.js',
+    },
+    resolve: {
+        root: path.resolve(__dirname),
+        extensions: ['', '.js', '.jsx'],
+        modulesDirectories: ['node_modules'],
+    },
+    module: {
+        loaders: [
+            { test: /\.js?$/, loaders: ['react-hot', 'babel'], exclude: /node_modules/ },
+            { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader'},
+            { test: /\.css$/, loader: "style!css" },
+            {test: /\.less/,loader: 'style-loader!css-loader!less-loader'}
+        ],
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+    ],
+};
